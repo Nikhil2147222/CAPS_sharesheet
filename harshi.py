dcfade77b2a19e8db10260cb15cb4267523bcbee
HARSHITHAJP <95636798+HARSHITHAJP@users.noreply.github.com>
2022-06-16 07:23:46 +0530

    Update harshi.py

--- harshi.py
+++ harshi.py
@@ -1,3 +1,108 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate 
+from django.contrib.auth.forms import AuthenticationForm 
+from django.contrib.auth.decorators import login_required
+
+from .forms import *
+from .models import rolestbl,campustbl,depttbl,coursetbl,User,stuhead_vol_profiletbl
+# from .decorators import unauthenticated_user
+
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+
+
+#---------------------------------------------------campus views-----------------------------------------------------------------
+def campusaddformfunc(request):
+    form =campusaddform()
+    temp = None
+    if campustbl.objects.exists():
+        temp = campustbl.objects.all()
+    
+    context = {'temp':temp, 'form':form}
+    if request.method == "POST":
+        form = campusaddform(request.POST)
+        if form.is_valid():
+            messages.success(request, "form saved")
+            a = form.save(commit = False)
+            a.save()
+            form = campusaddform()
+        else:
+            messages.error(request, "cannot add campus. Invalid information.")
+
+    return render (request,"campusaddform.html", context)
+
+def campusdeletefunc(request, pkid):
+    record=campustbl.objects.get(id=pkid)
+    print(record)
+    record.delete()
+    messages.success(request, "record deleted")
+    return redirect("campusaddform_url")
+
+
+def campusupdatefunc(request, pkid):
+    record=campustbl.objects.get(id=pkid)
+    form = campusaddform(instance=record)
+    if request.method == "POST":
+        form = campusaddform(request.POST,instance=record)
+        if form.is_valid():
+            messages.success(request, "form saved")
+            print("success")
+            a = form.save(commit = False)
+            a.save()
+            messages.success(request, "Campus form saved")
+        else:
+            messages.error(request, "cannot update campus. Invalid info.")
+        return redirect("campusaddform_url")
+    
+    context={'form':form}
+    return render (request,"campusupdateform.html", context)
+
+
+#---------------------------------------------------Session views-----------------------------------------------------------------
+  
+def success(request):
+    return HttpResponse('successfully uploaded')
+#---------------------------------------------------home views-----------------------------------------------------------------
+def homefunc(request):
+    return render(request, 'index.html',)
+
+#---------------------------------------------------home views-----------------------------------------------------------------
+def dashboardfunc(request):
+    return render(request, 'dashboard.html')
+#---------------------------------------------------LOGIN view-----------------------------------------------------------------
+# @unauthenticated_user
+def loginfunc(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"You are now logged in as {username}.")
+                return redirect("dashboard_url")
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    return render (request,"login.html", context={'form':form})
+
+
+def logoutfunc(request):
+    logout(request)
+    return redirect("login_url")
+
+
 #--------------------------------------------------- Department  views-----------------------------------------------------------------
 def deptaddformfunc(request):
     form =deptaddform()
@@ -14,6 +119,8 @@ def deptaddformfunc(request):
             form = deptaddform()
         else:
             messages.error(request, "cannot add dept. Invalid information.")
+        
+        return redirect("deptaddform_url")
     return render (request,"deptaddform.html", context)
 
 def deptdeletefunc(request, pkid):
@@ -41,6 +148,162 @@ def deptupdatefunc(request, pkid):
     
     context={'form':form}
     return render (request,"deptupdateform.html", context)
+
+
+
+#--------------------------------------------------- Course  views-----------------------------------------------------------------
+def courseaddformfunc(request):
+    form =courseaddform()
+    temp = None
+    if coursetbl.objects.exists():
+        temp = coursetbl.objects.all()
+    
+    context = {'temp':temp, 'form':form}
+    if request.method == "POST":
+        form = courseaddform(request.POST)
+        if form.is_valid():
+            messages.success(request, "form saved")
+            a = form.save(commit = False)
+            a.save()
+            form = courseaddform()
+        else:
+            messages.error(request, "cannot add course. Invalid information.")
+
+    return render (request,"courseaddform.html", context)
+
+def coursedeletefunc(request, pkid):
+    record=coursetbl.objects.get(id=pkid)
+    print(record)
+    record.delete()
+    messages.success(request, "record deleted")
+    return redirect("courseaddform_url")
+
+
+def courseupdatefunc(request, pkid):
+    record=coursetbl.objects.get(id=pkid)
+    form = courseaddform(instance=record)
+    if request.method == "POST":
+        form = courseaddform(request.POST,instance=record)
+        if form.is_valid():
+            messages.success(request, "form saved")
+            print("success")
+            a = form.save(commit = False)
+            a.save()
+            messages.success(request, "manga form saved")
+        else:
+            messages.error(request, "cannot update department. Invalid info.")
+        return redirect("courseaddform_url")
+    
+    context={'form':form}
+    return render (request,"courseupdateform.html", context)
+
+
+#-----------------------------------------------------------------------
+# Student head dashboard -----------------------------------------------------------
+def studenthead_dashboard_func(request):
+    
+    return render(request, 'studenthead/sh_dashboard.html')
+
+def sh_volunteer_list_func(request):
+    try:
+        if request.User.stuhead_vol_profiletbl.wing:
+            currentwing = request.User.stuhead_vol_profiletbl.wing
+            print(currentwing)
+
+        templist = stuhead_vol_profiletbl.objects.filter(wing=currentwing)
+        context = {'templist':templist,'currentwing':currentwing}
+        return render(request, 'volunteer_list.html', context)
+    except:
+        messages.error(request,"Unable to fetch data! sorry!")
+        return render(request, 'studenthead/sh_volunteer_list.html')
+
+def sh_volunteer_add_func(request):
+    form1 = UserAddForm
+    form2 = VolunteerAddForm
+
+    if request.method == "POST":
+        form1 = UserAddForm(request.POST)
+        form2 = VolunteerAddForm(request.POST)
+
+        if form1.is_valid() & form2.is_valid():
+            tempuser = form1.save(commit=False)
+            tempuser.roles = rolestbl.objects.filter(roles = 'volunteer').first()
+            print(tempuser.roles)
+            tempuser.save()
+            tempvol = form2.save(commit=False)
+            tempvol.user = tempuser
+            tempvol.save()
+
+            # send email code with pwd and OTP and link to login page
+
+            messages.success(request, "Volunteer registered successfully")
+            return redirect("sh_volunteer_list_url")
+        else:
+            print(form1.errors)
+            print(form2.errors)
+            messages.error(request, "Invalid data! please check the form.")
+
+    context = {"form1":form1,"form2":form2}
+    return render(request, 'studenthead/sh_volunteer_add.html',context)
+
+
+def sessionaddformfunc(request):
+    form =sessionaddform()
+    temp = None
+    if sessiontbl.objects.exists():
+        temp = sessiontbl.objects.all()
+    
+    context = {'temp':temp, 'form':form}
+    if request.method == "POST":
+        form = sessionaddform(request.POST, request.FILES)
+        if form.is_valid():
+            messages.success(request, "form saved")
+            a = form.save(commit = False)
+            a.save()
+            form = sessionaddform()
+        else:
+            messages.error(request, "cannot add session. Invalid information.")
+    return render (request,"sessionaddform.html", context)
+
+
+def sessiondeletefunc(request, pkid):
+    record=sessiontbl.objects.get(id=pkid)
+    print(record)
+    record.delete()
+    messages.success(request, "record deleted")
+    return redirect("sessionaddform_url")
+
+
+def sessionupdatefunc(request, pkid):
+    record=sessiontbl.objects.get(id=pkid)
+    form = sessionaddform(instance=record)
+    if request.method == "POST":
+        form = sessionaddform(request.POST,request.FILES,instance=record)
+        if form.is_valid():
+            messages.success(request, "form saved")
+            print("success")
+            a = form.save(commit = False)
+            a.save()
+            messages.success(request, "session form saved")
+        else:
+            messages.error(request, "cannot update session. Invalid info.")
+        return redirect("sessionaddform_url")
+    
+    context={'form':form}
+    return render (request,"sessionupdateform.html", context)
+
+
+def usersessionlistfunc(request):
+    if sessiontbl.objects.exists():
+        temp = sessiontbl.objects.all()
+        context = {'temp':temp}
+        return render(request, 'usersessionlist.html', context)
+    else:
+        if not sessiontbl.objects.exists():
+            return render(request, 'usersessionlist.html',)
+    return render(request, 'usersessionlist.html', context)
+
+
 
 
 #_______________________________________________________________accounts models_____________________________________________________________
